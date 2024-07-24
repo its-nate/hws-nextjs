38d75e95651f7400fab6b0c926ff0a17cc8b610e
Nate Micinski <nmicinski@gmail.com>
2024-07-24 14:56:32 -0700

    add custom fonts and colors

---
+++ src/utils/fonts.js
@@ -0,0 +1,4 @@
+import localFont from 'next/font/local'
+export const Montserrat = localFont({ src: './fonts/Montserrat/Montserrat-Regular.ttf', variable: '--font-montserrat' });
+export const OpenSans = localFont({ src: './fonts/Open_Sans/OpenSans-Regular.ttf', variable: '--font-openSans' });
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'dark-green': '#1b4a55',
+      'light-green': '#81c3c9',
+      'grey': '#a6a6a6',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
